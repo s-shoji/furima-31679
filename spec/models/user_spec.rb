5bd6bf89c2c8f591b9edbcd1f4ef9b4023dba0e2
shoji <s.shoji39@gmail.com>
2020-11-11 12:24:19 +0900

    テストコード修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -32,7 +32,6 @@ describe User do
         another_user = FactoryBot.build(:user)
         another_user.email = @user.email
         another_user.valid?
-
         expect(another_user.errors.full_messages).to include('Email has already been taken')
       end
       it 'passwordが空では登録できない' do
@@ -53,16 +52,19 @@ describe User do
         expect(@user.errors.full_messages).to include('Password には英字と数字の両方を含めて設定してください')
       end
       it 'passwordが存在してもpassword_confirmationが空では登録できない' do
-        # @user.password = "aaaaaaaaaa"
         @user.password_confirmation = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
-      it 'ユーザー本名は、名字と名前それぞれが必須である' do
+      it 'ユーザー本名は、名字が必須である' do
         @user.last_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name can't be blank")
+      end
+      it 'ユーザー本名は、名前が必須である' do
         @user.first_name = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("First name can't be blank", "Last name can't be blank")
+        expect(@user.errors.full_messages).to include("First name can't be blank")
       end
       it 'ユーザー本名は、全角（漢字・ひらがな・カタカナ）で入力させる' do
         @user.last_name = ''
