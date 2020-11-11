735c830c8c4270bb93188a4709618e4085a56920
shoji <s.shoji39@gmail.com>
2020-11-11 17:39:06 +0900

    アクティブハッシュの設定

---
+++ app/models/shipping_day.rb
@@ -0,0 +1,13 @@
+class ShippingDay < ApplicationRecord
+  ActiveHash::Base
+  self.data = [
+    { id: 0, name: '--' },   # ←こちらに注目しましょう
+    { id: 1, name: '1~2日で発送' },
+    { id: 2, name: '2~3日で発送' },
+    { id: 3, name: '4~7日で発送' }
+  ]
+
+  include ActiveHash::Associations
+  has_many :items
+
+end
