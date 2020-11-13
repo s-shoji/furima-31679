80f5b8606fd78850a16731d645e2fb06d8c1f664
shoji <s.shoji39@gmail.com>
2020-11-13 18:06:16 +0900

    新規投稿順に表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,7 @@ class ItemsController < ApplicationController
   
   def index
     @item = Item.new
-    @items = Item.includes(:user)
+    @items = Item.includes(:user).order("created_at DESC")
   end
 
 
