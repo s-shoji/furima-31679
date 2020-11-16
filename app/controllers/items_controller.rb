21acf7952b22c45a922d1d6ceff87f278d00158f
shoji <s.shoji39@gmail.com>
2020-11-16 17:01:53 +0900

    コントローラーの設定。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_item, only: [:edit, :show, :update]
+  before_action :move_to_index,  only: :edit
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -18,8 +20,22 @@ class ItemsController < ApplicationController
     end
   end
   def show
-    @item = Item.find(params[:id])
+    # @item = Item.find(params[:id])
   end
+
+  def edit
+    # @item = Item.find(params[:id])
+  end
+
+  def update
+     if @item.update(item_params)
+      redirect_to item_path(item.id)
+    else
+      render :edit
+    end
+
+  end
+
 
   private
 
@@ -28,4 +44,16 @@ class ItemsController < ApplicationController
                                  :prefecture_id, :shipping_day_id, :price, :image)
           .merge(user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless @item.user_id == current_user.id
+      redirect_to action: :index
+    end
+  end
+
 end
