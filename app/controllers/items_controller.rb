bf0ea8c48d208af3e0212ae661b22a27c30326c8
shoji <s.shoji39@gmail.com>
2020-11-16 20:24:58 +0900

    destroyアクションの記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,6 +36,14 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    item = Item.find(params[:id])
+    if item.destroy
+      redirect_to root_path
+    end
+  end
+
+
   private
 
   def item_params
