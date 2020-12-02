9e5434ef6abc8a9f24f3883b311e70d17736dc36
shoji <s.shoji39@gmail.com>
2020-12-02 10:13:55 +0900

    テストコード

---
+++ spec/factories/user_purchases.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user_purchase do
+    postal_code { '123-4567' }
+    prefecture_id { 1 }
+    city { '東京都' }
+    building_name { '東京ハイツ' }
+    address { '青山1-1-1' }
+    phone { '09012345678' }
+    purchase_id { 12 }
+    item_id { 5 }
+    token {"tok_abcdefghijk00000000000000000"}
+  end
+end
---
+++ spec/models/user_purchase_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+
+RSpec.describe UserPurchase, type: :model do
+  describe '購入の保存' do
+    before do
+      @user_purchase = FactoryBot.build(:user_purchase)
+    end
+
+    it 'すべての値が正しく入力されていれば保存できること' do
+      expect(@user_purchase).to be_valid
+    end
+    
+    it 'postal_codeが空だと保存できないこと' do
+      @user_purchase.postal_code = nil
+      @user_purchase.valid?
+      expect(@user_purchase.errors.full_messages).to include("Postal code can't be blank")
+    end
+    it 'postal_codeが半角のハイフンを含んだ正しい形式でないと保存できないこと' do
+      @user_purchase.postal_code = '1234567'
+      @user_purchase.valid?
+      expect(@user_purchase.errors.full_messages).to include("Postal code is invalid. Include hyphen(-)")
+    end
+    it 'prefectureを選択していないと保存できないこと' do
+      @user_purchase.prefecture_id = 0
+      @user_purchase.valid?
+      expect(@user_purchase.errors.full_messages).to include("Prefecture must be other than 0")
+    end
+    it 'cityが空だと保存できないこと' do
+      @user_purchase.city = nil
+      @user_purchase.valid?
+      expect(@user_purchase.errors.full_messages).to include("City can't be blank")
+    end
+    it 'building_nameは空でも保存できること' do
+      @user_purchase.building_name = nil
+      expect(@user_purchase).to be_valid
+    end
+    it 'addressが空だと保存できないこと' do
+      @user_purchase.address = nil
+      @user_purchase.valid?
+      expect(@user_purchase.errors.full_messages).to include("Address can't be blank")
+    end
+    it 'phoneが空だと保存できないこと' do
+      @user_purchase.phone = nil
+      @user_purchase.valid?
+      expect(@user_purchase.errors.full_messages).to include("Phone can't be blank")
+    end
+    it 'phoneにハイフンは不要で11桁以内でないと保存できないこと' do
+      @user_purchase.phone = "090-1234-5678"
+      @user_purchase.valid?
+      expect(@user_purchase.errors.full_messages).to include("Phone is invalid. Don't include hyphen(-)")
+    end
+    it "tokenが空では登録できないこと" do
+      @user_purchase.token = nil
+      @user_purchase.valid?
+      expect(@user_purchase.errors.full_messages).to include("Token can't be blank")
+    end
+
+  end
+end
